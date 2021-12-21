5e3dae8dd57e8c14a6f6e75a01ef4fcc6c8ca272
Mateus Silva Souza <mathheussilvasouza@gmail.com>
2021-12-21 00:00:13 -0300

    Construindo Scripts

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+function start(){ //Inicio da função Start
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador'></div>");
+    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo'></div>");
+
+} //Fim da função Start
